4fbf6ddf292afffb4b3de45855e1dc6cb867523c
Lana Holmes <lana.lee.holmes@gmail.com>
2019-01-05 00:50:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(int)
-  test_array = (2...int).to_a
+  test_array = (2...int.abs).to_a
   test_array.each do |number|
-    if (int % number) == 0
+    if (int.abs % number) == 0
       return false
     end
   end
